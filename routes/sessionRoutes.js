37ca050f278536a1f0e6560c89714577cc420185
ADITYASHANKER <122766579+ADITYASHANKER2801@users.noreply.github.com>
2025-08-17 11:48:48 +0530

    Update sessionRoutes.js

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { create, get } = require('../controllers/sessionController');
+const { create, get, getActiveSessions } = require('../controllers/sessionController');
 const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.post('/create', authMiddleware, create);
 router.get('/:roomId', authMiddleware, get);
-
+router.get('/active/dashboard', authMiddleware, roleMiddleware('TA'), getActiveSessions);
 module.exports = router;
