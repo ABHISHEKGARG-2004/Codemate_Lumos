329c748ac51bc07effbadf62015f9523f462a660
tayal0987 <abhishektayal0897@gmail.com>
2025-08-17 11:38:05 +0530

    added a route for getting a session

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -19,3 +19,15 @@ exports.create=async(req,res)=>{
         });
     }
 }
+
+exports.get = async (req, res) => {
+    try {
+        const session = await Session.findOne({ roomId: req.params.roomId }).populate('participants', 'username role');
+        if (!session) {
+            return res.status(404).json({ message: 'Session not found' });
+        }
+        res.json(session);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/sessionController');
+const { create, get } = require('../controllers/sessionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/create', authMiddleware, create);
-
+router.get('/:roomId', authMiddleware, get);
 
 module.exports = router;
